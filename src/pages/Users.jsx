0bc8e9a21da85ec4c8b5d16b2cf103ca6fd68647
Japhet <jaazbogati@gmail.com>
2026-04-04 18:35:14 +0200

    expanding users

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -4,37 +4,48 @@ import Navbar from "../components/Navbar";
 
 export default function Users() {
     const [users, setUsers] = useState([]);
+    const [loading , setLoading] = useState(true);
 
     useEffect(() => {
         API.get("/users")
-            .then(res => setUsers(res.data.data))
-            .catch(() => alert("Access denied"));
+            .then(res => {
+                setUsers(res.data.data);
+                setLoading(false);
+            })
+            .catch(() => {
+                alert("Access denied");
+                setLoading(false);
+            });
     }, [])
 
     return (
         <div>
             <Navbar />
-            <div>
+            <div className="p-6">
                 <h2 className="text-xl mb-4">Users</h2>
 
-                <table className="w-full border">
-                    <thead>
-                        <tr>
-                            <th>Email</th>
-                            <th>Role</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {users.map(u => (
-                            <tr key={u.id}>
-                                <td>{u.email}</td>
-                                <td>{u.role}</td>
-                                <td>{u.is_active ? "Active" : "Inactive"}</td>
+                {loading ? (
+                    <p>Loading...</p>
+                ) : (
+                    <table className="w-full border">
+                        <thead>
+                            <tr className="bg-gray-100">
+                                <th className="p-2 border">Email</th>
+                                <th className="p-2 border">Role</th>
+                                <th className="p-2 border">Status</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {users.map(u => (
+                                <tr key={u.id}>
+                                    <td className="p-2 border">{u.email}</td>
+                                    <td className="p-2 border">{u.role}</td>
+                                    <td className="p-2 border">{u.is_active ? "Active" : "Inactive"}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                   </table>
+                )}
             </div>
         </div>
     );
